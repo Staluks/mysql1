3b6a5e17b8cff8b3a05b5dddc55205319792abb0
Staluks <janchukssss@inbox.lv>
2022-07-19 10:51:43 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         DBlogic db = new DBlogic();
         System.out.println("Insert(i)/Delete(d)/Update(u)/Select all list(s)");
         String action = scan.next();
+        while(!action.toLowerCase().equals("i") && !action.toLowerCase().equals("d") && !action.toLowerCase().equals("u") && !action.toLowerCase().equals("s")) {
+            System.out.println("please select from given choices!");
+            System.out.println("Insert(i)/Delete(d)/Update(u)/Select all list(s)");
+            action  = scan.next();
+        }
         switch (action.toLowerCase()) {
             case "i":
                 db.getInsert();
